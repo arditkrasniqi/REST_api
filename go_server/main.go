7c5fd86900d9eac04d091332aa6ca082a4ec94b4
arditkrasniqi <krasniqiardit01@gmail.com>
2018-04-04 11:06:38 +0200

    CRUD form

--- go_server/main.go
+++ go_server/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"database/sql"
 	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
 
@@ -12,6 +13,7 @@ import (
 
 var people []Person
 var users []user
+var db, dberror = sql.Open("mysql", "root:krasniqi01@tcp(localhost:3306)/test")
 
 type Person struct {
 	ID        string   `json:"id,omitempty"`
@@ -32,19 +34,10 @@ type user struct {
 }
 
 func GetPersonEndpoint(w http.ResponseWriter, req *http.Request) {
-	w.Header().Set("Access-Control-Allow-Origin", req.Header.Get("Origin"))
-	w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
 	json.NewEncoder(w).Encode(people)
 }
 
-func GetUsersEndPoint(w http.ResponseWriter, req *http.Request) {
-	w.Header().Set("Access-Control-Allow-Origin", req.Header.Get("Origin"))
-	w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
-	db, err := sql.Open("mysql", "root:krasniqi01@tcp(localhost:3306)/test")
-	err = db.Ping()
-	if err != nil {
-		log.Fatal(err)
-	}
+func GetUsersEndpoint(w http.ResponseWriter, req *http.Request) {
 	rows, err := db.Query("SELECT * FROM users")
 	if err != nil {
 		log.Fatal(err)
@@ -56,13 +49,49 @@ func GetUsersEndPoint(w http.ResponseWriter, req *http.Request) {
 		}
 		users = append(users, model)
 	}
+	defer rows.Close()
 	json.NewEncoder(w).Encode(users)
 }
 
+var results []string
+
+func InsertUserEndpoint(w http.ResponseWriter, req *http.Request) {
+	_, err := db.Query("insert into users(name, email) values('" + req.FormValue("name") + "','" + req.FormValue("email") + "')")
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Fprint(w, "Inserted")
+}
+
+func DeleteUserEndpoint(w http.ResponseWriter, req *http.Request) {
+	params := mux.Vars(req)
+	_, err := db.Query("delete from users where id = " + params["id"])
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Fprint(w, "Deleted")
+}
+
+func UpdateUserEndpoint(w http.ResponseWriter, req *http.Request) {
+	params := mux.Vars(req)
+	_, err := db.Query("update users set name = '" + req.FormValue("name") + "', email = '" + req.FormValue("email") + "' where id = " + params["id"])
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Fprint(w, "Updated")
+}
+
 func main() {
+	dberror = db.Ping()
+	if dberror != nil {
+		log.Fatal(dberror)
+	}
 	router := mux.NewRouter()
 	people = append(people, Person{ID: "1", Firstname: "Ardit", Lastname: "Krasniqi", Address: &Address{City: "Damanek", State: "Kosova"}})
 	router.HandleFunc("/people", GetPersonEndpoint).Methods("GET")
-	router.HandleFunc("/users", GetUsersEndPoint).Methods("GET")
+	router.HandleFunc("/users", GetUsersEndpoint).Methods("GET")
+	router.HandleFunc("/new-user", InsertUserEndpoint).Methods("POST")
+	router.HandleFunc("/delete-user/{id}", DeleteUserEndpoint).Methods("DELETE")
+	router.HandleFunc("/update-user/{id}", UpdateUserEndpoint).Methods("PATCH")
 	log.Fatal(http.ListenAndServe(":12345", router))
 }
